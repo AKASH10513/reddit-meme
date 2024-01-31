76e6a9d0a0ed2c83043c139886f6dfef47a19dfd
AKASH10513 <akashksinha7115@gmail.com>
2024-01-31 17:28:45 +0530

    final commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import HomePage from "@/pages/homePage/HomePage"
+import "./globals.css"
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="heading">
+      <h1>Reddit Gallery</h1>
+      </div>
+      <HomePage/>
+    </div>
+  )
+}
